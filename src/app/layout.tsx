d7d7f5d3ecead4abb5e7b78e765836577bc27edb
paella <paella@paellas-Mac-Pro.local>
2024-06-29 23:24:28 +0200

    added test navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,14 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return(
+    <nav className="flex items-center justify-between w-full bg-gradient-to-b from-[#15162c] to-[#2e026d] text-white shadow-lg">
+      <div>Prueba</div>
+    </nav>
+  )
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -15,7 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
